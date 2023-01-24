bb9e4418e5ccb0abe21fb1f5b12c2103d4a442cd
Max <the64man1@yahoo.com>
2023-01-24 17:02:33 -0600

    recipe list setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('t', 't', '')
+  ];
+  constructor() {}
+  ngOnInit() {
+  }
 }
