ce5c84e1efda96f7f525534903e2cbd5b92f20de
Max <the64man1@yahoo.com>
2023-02-01 11:47:28 -0600

    procfile

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ShoppingListApp'));
+app.use(express.static("./dist/project-name"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/ShoppingListApp/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/project-name" });
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
