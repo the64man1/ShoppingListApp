87500491e032205bec86432e351d6f73652ae467
Max <the64man1@yahoo.com>
2023-02-01 11:50:51 -0600

    app name server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/project-name"));
+app.use(express.static("./dist/shopping-list-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/project-name" });
+  res.sendFile("index.html", { root: "dist/shopping-list-app" });
 });
 
 app.listen(process.env.PORT || 8080);
